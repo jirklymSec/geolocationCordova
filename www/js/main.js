198cb22f1dbedcd22987e0ac02fd889ae253c199
jiri.korenek@initedsolutions.cz <jiri.korenek@initedsolutions.cz>
2018-11-23 11:41:25 +0100

    basic geolocation functionality

--- www/js/main.js
+++ www/js/main.js
@@ -1,4 +1,18 @@
 function GetLocation()
 {
     alert("I have something");
+    navigator.geolocation.getCurrentPosition(this.succesCallback,this.errorCallback);
+}
+
+function succesCallback(position)
+{
+    alert("succes");
+    alert(position.coords.latitude + "\n" +
+    position.coords.longitude);
+    document.getElementById("map").src = "http://staticmap.openstreetmap.de/staticmap.php?center=" + position.coords.latitude + "," + position.coords.longitude +"&zoom=14&size=865x512&maptype=mapnik";    
+}
+
+function errorCallback()
+{
+    alert("error");
 }
